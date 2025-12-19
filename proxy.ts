2312c0d71ea98e2f4681acfacf2b331b73e4fa92
toby <hello@tobybelhome.com>
2025-12-20 01:21:02 +0300

    proxy

---
+++ proxy.ts
@@ -0,0 +1,8 @@
+import { NextResponse, type NextRequest } from "next/server";
+export function proxy(request: NextRequest) {
+  return NextResponse.redirect(new URL("/dashboard", request.url));
+}
+
+export const config = {
+  matcher: ["/dashboard", "/"]
+};
